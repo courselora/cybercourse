0366f7207345e2a2909ac76680f0c963d13c18d3
iblowmymind <daghan.tinaztepe@gmail.com>
2022-01-30 11:26:00 +0300

    Reboot

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cybercourse'
+copyright = '2021-2022, blw'
+author = 'blw'
 
 release = '0.1'
 version = '0.1.0'
