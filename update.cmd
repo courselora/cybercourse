ccf26535039a1ae705d8b187724e05d69f61c9d5
iblowmymind <daghan.tinaztepe@gmail.com>
2022-03-06 16:04:14 +0300

    Check the changed files for details.

---
+++ update.cmd
@@ -0,0 +1,6 @@
+@echo off
+rem just an auto-update thing cuz i'm lazy
+
+git add --all
+git commit -m "Check the changed files for details."
+git push
---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#just an auto-update thing cuz i'm lazy
+
+git add --all
+git commit -m "Check the changed files for details."
+git push
